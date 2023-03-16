cbcfd33b02c27e79883cfd1559cd124e39673a2f
Ido <Ido325935195>
2023-03-16 21:53:24 +0200

    Stashing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="screen">
+        <div className="column">
+          {/* <div className="profile">
+            <img className="profilePic" src="https://media.istockphoto.com/id/1322275410/photo/profile-of-young-man.jpg?b=1&s=170667a&w=0&k=20&c=86FMyZeMtHqqcudnTIf0s8_QqMD2AZpio0O_1f76w5c=" alt="ProfilePic"/>
+            <span className="onlineStatus"></span>
+          </div> */}
+        </div>
+        <div className="column">
+          <div className="chat"></div>
+        </div>
+      </div>
     </div>
   );
 }
