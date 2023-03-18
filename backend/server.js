829f97b500b3beb272536cfb519088ad933a169b
alona <alona.daphna@gmail.com>
2023-03-18 13:56:25 +0200

    connected to db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 
 const port = process.env.PORT
 const app = express()
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to database')
+});
 
 app.get('/', (req, res) => {
     res.send('hi')
