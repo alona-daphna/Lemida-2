4cdb18f60c2a55f6324e06ffa37e30b76d90e39f
alona <alona.daphna@gmail.com>
2023-04-04 16:38:39 +0300

    define models

--- backend/models/chatModule.js
+++ backend/models/chatModule.js
@@ -3,9 +3,31 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const chatSchema = new Schema({
-    // here we define our props
+    name: {
+        type: String,
+        required: true
+    },
+    members: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    message_history: {
+        text: {
+            type: String,
+            required: true
+        },
+        sender: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            required: true
+        }
+    }
 
-})
+}, {timestamps: true})
 
-// we export a Model object of mongoose and we need to pass it our schema and give it a name.
 module.exports = mongoose.model('Chat', chatSchema)
