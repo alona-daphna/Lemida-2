c4d9e80861182f19e74d7bb3127bda65269fc9fe
alona <alona.daphna@gmail.com>
2023-04-07 19:57:08 +0300

    send user back to client

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,13 +3,13 @@ const User = require('../models/userModule')
 
 // get a single user
 const getUser = async (req, res) => {
-    const user = await User.findById(req.params.id).select({ username: 1 });
+    const user = await User.findById(req.params.id).select({ password: 0 });
     res.status(200).json(user);
 }
 
 // get a list of all users
 const getUsers = async (req, res) => {
-    const users = await User.find({}).select({ username: 1 });
+    const users = await User.find({}).select({ password: 0 });
     res.status(200).json(users);
 }
 
@@ -17,17 +17,19 @@ const getUsers = async (req, res) => {
 const updateUser = async (req, res) => {
     const { username, password } = req.body;
 
-    const response = await User.updateOne(
+    const user = await User.findOneAndUpdate(
         { _id: req.params.id },
-        { username, password }
-    );
-    res.status(200).json('User updated');
+        { username, password },
+        // return the updated user
+        {new: true}
+    ).select({ password: 0 });
+    res.status(200).json(user);
 }
 
 // delete user
 const deleteUser = async (req, res) => {
-    const response = await User.deleteOne({ _id: req.params.id });
-    res.status(200).json('User deleted');
+    const user = await User.findOneAndDelete({ _id: req.params.id });
+    res.status(200).json(user);
 }
 
 module.exports = {
