ea3b588ad8b85a59f4a4b336dc9fb7accdbe26fe
alona <alona.daphna@gmail.com>
2023-04-08 10:50:31 +0300

    added html & css to register page

--- frontend/src/pages/register/Register.js
+++ frontend/src/pages/register/Register.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './register.css'
 
 const Register = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`)
+  }
+
   return (
-    <div>Register</div>
+    <div className='register-container'>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder='username'
+          value={username} 
+          onChange={(e) => setUsername(e.target.value)} />
+        <input 
+          type="text" 
+          placeholder='password'
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} />
+        <button className='register-button'>Register</button>
+      </form>
+    </div>
   )
 }
 
