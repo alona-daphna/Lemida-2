e8ff21c3b25a877d258e84638b032f4eddef85de
alona <alona.daphna@gmail.com>
2023-04-08 11:08:14 +0300

    add login html

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = (props) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`)
+  }
+
   return (
-    <div>Login</div>
+    <div className='register-container'>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder='username'
+          value={username} 
+          onChange={(e) => setUsername(e.target.value)} />
+        <input 
+          type="password" 
+          placeholder='password'
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} />
+        <button className='register-button'>Login</button>
+      </form>
+      
+      <span><Link to="/register">New to our app?</Link></span>
+    </div>
   )
 }
 
