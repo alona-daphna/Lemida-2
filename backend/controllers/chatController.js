7c97455131d7726517cb216de64c89be29487802
alona <alona.daphna@gmail.com>
2023-04-10 12:24:19 +0300

    add update chat and create message in chat functionality

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,5 +1,24 @@
 const User = require('../models/userModule')
 const Chat = require('../models/chatModule')
+const mongoose = require('mongoose')
+
+// move to utils
+const validateIds = async (ids, collection, object) => {
+    // check that ids are valid mongoose object ids
+    if (!ids.every(id => mongoose.Types.ObjectId.isValid(id))) {
+        return { status: 400, error: `Invalid ${object} id(s)` };object
+      }
+
+    // check that all ids exist in the specified collection
+    const memberObjs = await collection.find({ _id: { $in: ids } }, '_id');
+    if (memberObjs.length !== ids.length) {
+      return { status: 400, error: `${object} id(s) do(es) not exist in collection` };
+    }
+
+    return {status: 200};
+
+}
+
 
 // get details for a single chat including participants and chat history
 const getChat = async (req, res) => {
@@ -21,8 +40,12 @@ const getUserChats = async (req, res) => {
     try {
         // get the logged in user id
         const userId = req.userId;
+        console.log("user ID:", userId)
         // find all chats where the members array contains the user id
-        const chats = await Chat.find({ members: userId })
+        const chats = await Chat.find({ members: userId }).populate({
+            path: 'members',
+            select: '-password'
+        })
         return res.status(200).json(chats)
     } catch (error) {
         console.log(error)
@@ -44,13 +67,10 @@ const createChat = async (req, res) => {
     // add the user creating the chat to the members array
     members.push(req.userId);
 
-    try {
-        // check that all members are valid user ids
-        const memberObjs = await User.find({ _id: { $in: members } }, '_id');
-        if (memberObjs.length !== members.length) {
-            return res.status(400).json({ error: 'Invalid member id(s)' });
-        }
+    const { status, error } = await validateIds(members, User, 'member');
+    if (status === 400) return res.status(400).json({ error })
 
+    try {
         const chat = await Chat.create({ name, members });
         const populatedChat = await Chat.findById(chat._id).populate({
             path: 'members',
@@ -65,19 +85,86 @@ const createChat = async (req, res) => {
 
 // update chat name, list of participants
 const updateChat = async (req, res) => {
+    const {name, members} = req.body
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid chat ID.' });
+    }
 
+    if (!name) return res.status(400).json({error: 'Please provide a new chat name'})
+    if (!members) return res.status(400).json({error: 'Please provide an updated members array'})
+
+    // add current logged in user to members array
+    if (!members.includes(req.userId)) members.push(req.userId)
+
+    const { status, error } = await validateIds(members, User, 'member');
+    if (status === 400) return res.status(400).json({ error })
+
+    try {
+        const chat = await Chat.findOneAndUpdate(
+            {_id: req.params.id},
+            {name, members},
+            {new: true}
+        ).populate({
+            path: 'members',
+            select: '-password'
+        })
+
+        if (!chat) return res.status(400).json({error: 'Chat not found'})
+    
+        return res.status(200).json(chat)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({error: 'Failed to update chat'})
+    }
 }
 
 // create a new message in the specified chat 
 const createMessage = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid chat ID.' });
+    }
+
+    const { text } = req.body;
+    const sender = req.userId;
+    const newMessage = {
+        sender,
+        text,
+        createdAt: new Date()
+    };
+
+    if (!text) return res.status(400).json({error: 'Please provide message content'})
+
+    try {
+        const chat = await Chat.findOneAndUpdate(
+            {_id: req.params.id},
+            {$push: {message_history: newMessage}},
+            {new: true}
+        ).populate({
+            path: 'members',
+            select: '-password'
+        });
+        if (!chat) return res.status(400).json({error: 'Chat not found'})
+
+        return res.status(201).json(chat)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({error: 'Failed to create message'})
+    }
+
 
 }
 
 // delete chat
 const deleteChat = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid chat ID.' });
+    }
     try {
         const chatId = req.params.id;
-        const chat = await Chat.findOneAndDelete({_id: chatId})
+        const chat = await Chat.findOneAndDelete({_id: chatId}).populate({
+            path: 'members',
+            select: '-password'
+        })
         res.status(200).json(chat)
     } catch (error) {
         console.log(error)
