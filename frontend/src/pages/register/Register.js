3ff8490512951a984caa5666362a3e4c85ba080c
alona <alona.daphna@gmail.com>
2023-04-12 17:02:16 +0300

    add loading page in login and register

--- frontend/src/pages/register/Register.js
+++ frontend/src/pages/register/Register.js
@@ -2,22 +2,24 @@ import React, { useContext, useState } from 'react'
 import './register.css'
 import { Link, useNavigate } from 'react-router-dom'
 import { UserContext } from '../../context/userContext'
+import Loading from '../loading/Loading'
 
 
 const Register = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
   const navigate = useNavigate()
-  const {setUser} = useContext(UserContext)
+  const { setUser } = useContext(UserContext)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(`Username: ${username}, Password: ${password}`)
+    setIsLoading(true)
 
     const response = await fetch('http://localhost:4000/api/auth/register', {
       method: 'POST',
-      body: JSON.stringify({username, password}),
+      body: JSON.stringify({ username, password }),
       headers: {
         'Content-Type': 'application/json'
       },
@@ -30,19 +32,22 @@ const Register = () => {
     if (response.ok) {
       const response2 = await fetch('http://localhost:4000/api/auth/login', {
         method: 'POST',
-        body: JSON.stringify({username, password}),
+        body: JSON.stringify({ username, password }),
         headers: {
           'Content-Type': 'application/json'
         },
-      credentials: 'include'
+        credentials: 'include'
       })
       if (response2.ok) {
         setUser(json)
-        navigate('/')
         setUsername('')
         setPassword('')
         setError('')
-        } 
+        setTimeout(() => {
+          setIsLoading(false)
+          navigate('/')
+        }, 1500)
+      }
     } else {
       setError(json.error)
     }
@@ -50,26 +55,29 @@ const Register = () => {
 
   return (
     <div className='register-container'>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          placeholder='username'
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)} />
-        <input 
-          type="password" 
-          placeholder='password'
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} />
-        <button className='register-button'>Register</button>
-      </form>
-      
-      <span className='already-exist'><Link to="/login">Already have an account?</Link></span>
-      {error && 
-      <span className="register-error">{error}</span>
+      {isLoading ? <Loading /> :
+        <>
+          <h1>Register</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder='username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)} />
+            <input
+              type="password"
+              placeholder='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+            <button className='register-button'>Register</button>
+          </form>
+
+          <span className='already-exist'><Link to="/login">Already have an account?</Link></span>
+          {error &&
+            <span className="register-error">{error}</span>
+          }
+        </>
       }
-      
     </div>
   )
 }
