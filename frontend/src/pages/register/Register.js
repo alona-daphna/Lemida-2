d499e6d050479a283ee15a153d06848586918246
alona-daphna <74145815+alona-daphna@users.noreply.github.com>
2023-04-13 19:11:07 +0300

    Update Register.js

--- frontend/src/pages/register/Register.js
+++ frontend/src/pages/register/Register.js
@@ -2,20 +2,18 @@ import React, { useContext, useState } from 'react'
 import './register.css'
 import { Link, useNavigate } from 'react-router-dom'
 import { UserContext } from '../../context/userContext'
-import Loading from '../loading/Loading'
 
 
 const Register = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
   const navigate = useNavigate()
   const { setUser } = useContext(UserContext)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setIsLoading(true)
+    console.log(`Username: ${username}, Password: ${password}`)
 
     const response = await fetch('http://localhost:4000/api/auth/register', {
       method: 'POST',
@@ -40,13 +38,10 @@ const Register = () => {
       })
       if (response2.ok) {
         setUser(json)
+        navigate('/')
         setUsername('')
         setPassword('')
         setError('')
-        setTimeout(() => {
-          setIsLoading(false)
-          navigate('/')
-        }, 1500)
       }
     } else {
       setError(json.error)
@@ -55,29 +50,26 @@ const Register = () => {
 
   return (
     <div className='register-container'>
-      {isLoading ? <Loading /> :
-        <>
-          <h1>Register</h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder='username'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)} />
-            <input
-              type="password"
-              placeholder='password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)} />
-            <button className='register-button'>Register</button>
-          </form>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder='username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)} />
+        <input
+          type="password"
+          placeholder='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} />
+        <button className='register-button'>Register</button>
+      </form>
 
-          <span className='already-exist'><Link to="/login">Already have an account?</Link></span>
-          {error &&
-            <span className="register-error">{error}</span>
-          }
-        </>
+      <span className='already-exist'><Link to="/login">Already have an account?</Link></span>
+      {error &&
+        <span className="register-error">{error}</span>
       }
+
     </div>
   )
 }
