72c430054b5cf665eeb60c2743a3e3a98a970fed
alona <alona.daphna@gmail.com>
2023-04-14 10:09:04 +0300

    clean up when logging out

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,9 +1,13 @@
 import React, { useContext } from 'react'
 import './navbar.css'
 import { UserContext } from '../../context/userContext';
+import { ChosenChatContext } from '../../context/chosenChatContext';
+import { ChatContext } from '../../context/chatListContext';
 
 const Navbar = () => {
   const { user, setUser } = useContext(UserContext)
+  const { setChosenChat } = useContext(ChosenChatContext)
+  const { dispatch: setChatlist } = useContext(ChatContext)
 
   const handleLogout = async () => {
     const response = await fetch('http://localhost:4000/api/auth/logout', {
@@ -11,6 +15,11 @@ const Navbar = () => {
     })
     if (response.ok) {
       setUser(null)
+      setChosenChat(null)
+      setChatlist({
+        type: 'SET_CHATS',
+        payload: []
+      })
     } else {
       console.log('Failed to logout')
     }
