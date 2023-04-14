cd3e068ac2f604d4f2abf8af0f88f0f5af028319
alona <alona.daphna@gmail.com>
2023-04-14 09:43:20 +0300

    set condition for isMine prop

--- frontend/src/components/chat/Chat.js
+++ frontend/src/components/chat/Chat.js
@@ -4,9 +4,11 @@ import { IoSend, IoArrowBack } from 'react-icons/io5';
 import { CgMoreVerticalAlt, CgSearch } from 'react-icons/cg';
 import { useState, useRef, useEffect, useContext } from 'react';
 import { ChosenChatContext } from '../../context/chosenChatContext';
+import { UserContext } from '../../context/userContext';
 
 const Chat = ({ onBackClick }) => {
-    const {chosenChat} = useContext(ChosenChatContext)
+    const { chosenChat } = useContext(ChosenChatContext)
+    const { user } = useContext(UserContext)
 
     const [message, setMessage] = useState('')
 
@@ -37,11 +39,11 @@ const Chat = ({ onBackClick }) => {
         const scrollTop = chatBodyRef.current.scrollTop
         const scrollHeight = chatBodyRef.current.scrollHeight
         const clientHeight = chatBodyRef.current.clientHeight
-        
+
         // thershold is set to about one message
         return Math.abs(scrollHeight - clientHeight - scrollTop) < 50
     }
-    
+
     useEffect(() => {
         if (userAtBottom) {
             scrollToBottom()
@@ -51,71 +53,85 @@ const Chat = ({ onBackClick }) => {
     useEffect(() => {
         // fetch chat message history
         const fetchMessages = async () => {
-            const response = await fetch(`http://localhost:4000/api/chats/${chosenChat}`, {
-                credentials: 'include'
-            })
-            const chat = await response.json()
-            console.log(chat.message_history)
-            setMessages(chat.message_history)
+            if (chosenChat) {
+
+                const response = await fetch(`http://localhost:4000/api/chats/${chosenChat}`, {
+                    credentials: 'include'
+                })
+                const chat = await response.json()
+                console.log(chat.message_history)
+                setMessages(chat.message_history)
+            }
         }
 
         fetchMessages()
     }, [chosenChat])
-    
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (message) {
-            setUserAtBottom( isScrolledToBottom() )
-            setMessages([...messages, {text: message, sent: true}])
+            setUserAtBottom(isScrolledToBottom())
+            setMessages([...messages, { text: message, sender: user._id }])
+
+
+            // save message to db
+            const response = await fetch(`http://localhost:4000/api/chats/${chosenChat}/messages`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    "text": message
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include'
+            })
+
+            console.log(await response.json())
+
+            setMessage('')
         }
 
-        // save message to db
-        const response = await fetch(`http://localhost:4000/api/chats/${chosenChat}/messages`, {
-            method: 'POST',
-            body: JSON.stringify({
-                "text":  message
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include'
-        })
-
-        console.log(await response.json())
-
-        setMessage('')
-
     }
-    return ( 
+    return (
         <div className="chat">
-            <div className="top-bar">
-                <button className='back' onClick={onBackClick}><IoArrowBack /></button>
-                <img className='profile-picture' src="https://shorturl.at/dfpzV" alt="ProfilePic"/>
-                <div className='info'>
-                    <p className='chat-name'>MERN Lemida Project 🤓</p>
-                    <p className='chat-info'>Tap here for group info</p>
-                </div>
-                <CgSearch className='search-chat-button' />
-                <CgMoreVerticalAlt className='more-button' />
-            </div>
-            
-            <div className="chat-body" ref={chatBodyRef}>
-                {messages && messages.map((message, index) => (
-                    <Message 
-                        key={index} 
-                        message={message.text} 
-                        // isMine={message.sent} 
-                    />
-                ))}
-                <div ref={messagesEndRef} /> 
-            </div>
+            {chosenChat ?
+                <>
+                    <div className="top-bar">
+                        <button className='back' onClick={onBackClick}><IoArrowBack /></button>
+                        <img className='profile-picture' src="https://shorturl.at/dfpzV" alt="ProfilePic" />
+                        <div className='info'>
+                            <p className='chat-name'>MERN Lemida Project 🤓</p>
+                            <p className='chat-info'>Tap here for group info</p>
+                        </div>
+                        <CgSearch className='search-chat-button' />
+                        <CgMoreVerticalAlt className='more-button' />
+                    </div>
 
-            <form className="bottom-bar" onSubmit={handleSubmit}>
-                <input className='message' type="text" placeholder='Type a message' value={message} onChange={(e) => setMessage(e.target.value)}/>
-                <button className="send"><IoSend /></button>
-            </form>
+                    <div className="chat-body" ref={chatBodyRef}>
+                        {messages && messages.map((message, index) => (
+                            <Message
+                                key={index}
+                                message={message.text}
+                                isMine={message.sender === user._id}
+                            />
+                        ))}
+                        <div ref={messagesEndRef} />
+                    </div>
+
+                    <form className="bottom-bar" onSubmit={handleSubmit}>
+                        <input className='message' type="text" placeholder='Type a message' value={message} onChange={(e) => setMessage(e.target.value)} />
+                        <button className="send"><IoSend /></button>
+                    </form>
+                </>
+                :
+                <>
+                    <div className="no-chosen-chat">
+                        <h3>welcome</h3>
+                    </div>
+                </>
+            }
         </div>
     );
 }
- 
+
 export default Chat;
