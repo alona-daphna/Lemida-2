ae453253c9cc130fab9d183fa0600beeccc8b9ad
Ido <Ido325935195>
2023-04-16 22:07:26 +0300

    Added time, sender, and checkmarks

--- frontend/src/components/message/Message.js
+++ frontend/src/components/message/Message.js
@@ -1,9 +1,17 @@
 import './message.css'
 
-const Message = ({ message, isMine }) => {
+const Message = ({ message, isMine, time, username }) => {
+    const date = new Date(time);
+    const hour = date.getHours().toString().padStart(2, "0");
+    const minute = date.getMinutes().toString().padStart(2, "0");
     return (
-        <div className={isMine ? 'message-bubble mine' : 'message-bubble'}>
-            <p>{message}</p>
+        <div className={"message-bubble " + (isMine ? "sent-message" : "received-message")}>
+            <p className="sender-name">{isMine ? "You" : username}</p>
+            <p className="message-text">{message}</p>
+            <div className="msg-info">
+                <p className="message-time">{`${hour}:${minute}`}</p>
+                <p className="checkmarks"></p>
+            </div>
         </div>
     );
 }
