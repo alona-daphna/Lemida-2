b1849b24bc29c970e525afa43934037505d033d0
Ido <Ido325935195>
2023-04-18 22:50:03 +0300

    Added sender name to conversations after refresh

--- frontend/src/utils/formatChat.js
+++ frontend/src/utils/formatChat.js
@@ -1,5 +1,12 @@
 export const formatChat = (chat) => {
+    const idToUsername = {};
+    chat.members.forEach(member => {
+        idToUsername[member._id] = member.username;
+    });
     const lastMessage = chat.message_history.slice(-1)[0];
+    const { sender, text, createdAt } = lastMessage ? lastMessage : ["", "", ""];
+    const username = idToUsername[sender];
+
     const date = lastMessage ? new Date(lastMessage.createdAt) : "";
     const hour = date ? date.getHours().toString().padStart(2, "0") : "";
     const minute = date ? date.getMinutes().toString().padStart(2, "0") : "";
@@ -7,6 +14,7 @@ export const formatChat = (chat) => {
         "id": chat._id,
         "name": chat.name,
         "lastMsg": lastMessage ? lastMessage.text : "",
+        "senderName": username,
         "time": date ? `${hour}:${minute}` : "",
         // To change to real images
         "picture": chat.name==="Ido" ? "https://shorturl.at/dfpzV" : "",
